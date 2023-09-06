b32baa8ee97e5cd564ea648a3910e2193fc89251
min <gkakgkdlf8@naver.com>
2023-09-06 16:28:14 +0900

    mac os

---
+++ src/main/java/com/example/shortapitest/ShortApiTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.shortapitest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShortApiTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShortApiTestApplication.class, args);
+	}
+
+}
