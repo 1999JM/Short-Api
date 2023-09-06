53d956146aa641535338fd2af2d0150953714909
min <gkakgkdlf8@naver.com>
2023-09-06 17:18:48 +0900

    mac os

---
+++ src/main/java/com/example/shortapitest/access/entity/Url.java
@@ -0,0 +1,28 @@
+package com.example.shortapitest.access.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+import java.util.Date;
+
+@Entity
+public class Url {
+
+    @Id
+    private String shortUrl;
+
+    @Column(nullable = false)
+    private String destination_url;
+    private String short_url;
+
+    @Column(nullable = false)
+    private Date created_date;
+
+    @Column(nullable = false)
+    private int total_clicks ;
+    @Column(nullable = false)
+    private int lest_clicked;
+
+
+}
