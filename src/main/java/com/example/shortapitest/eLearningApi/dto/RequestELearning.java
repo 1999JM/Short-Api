6c71d78c58e7d7c73f41e963ca0eb3bba3fe583c
min <gkakgkdlf8@naver.com>
2023-09-08 17:23:14 +0900

    mac os

---
+++ src/main/java/com/example/shortapitest/eLearningApi/dto/RequestELearning.java
@@ -0,0 +1,14 @@
+package com.example.shortapitest.eLearningApi.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class RequestELearning {
+
+    private String eLearningName;
+
+    private String eLearningAlias;
+
+}
