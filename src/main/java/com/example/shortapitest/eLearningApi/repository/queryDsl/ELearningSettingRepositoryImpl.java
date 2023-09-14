aaedfc8704e445b47a7458d535713f7393f689b8
min <gkakgkdlf8@naver.com>
2023-09-15 02:39:41 +0900

    mac os

--- src/main/java/com/example/shortapitest/eLearningApi/repository/queryDsl/ELearningSettingRepositoryImpl.java
+++ src/main/java/com/example/shortapitest/eLearningApi/repository/queryDsl/ELearningSettingRepositoryImpl.java
@@ -25,13 +25,18 @@ public class ELearningSettingRepositoryImpl implements ELearningSettingRepositor
     private final JPAQueryFactory queryFactory;
 
     @Override
-    public PageImpl<ELearningSetting> selectELearningSetting(Pageable pageable) {
+    public Page<ResponseSetting> selectELearningSetting(Pageable pageable) {
         //찾아본 결과 fetchResults() 향후 사라질 기능으로 조회 쿼리와 총 개수를 찾는 쿼리를 분리하여 작성
         QELearningSetting eLearningSetting = QELearningSetting.eLearningSetting;
-        List<ELearningSetting> content = queryFactory
+        List<ResponseSetting> content = queryFactory
                 // 리스트로 리턴해줘야 하는 값들 이러닝 네임, 이러닝 별칭, 이러닝, 삭제 처리 여부
-                .selectFrom(eLearningSetting)
-                .orderBy(eLearningSetting.createDate.desc())
+                .select(new ResponseSetting(
+                        eLearningSetting.id.as("")
+                        QELearningSetting.eLearningSetting.name,
+                        QELearningSetting.eLearningSetting.alias,
+                        QELearningSetting.eLearningSetting.deleted))
+                .from(QELearningSetting.eLearningSetting)
+                .orderBy(QELearningSetting.eLearningSetting.createDate.desc())
                 .offset(pageable.getOffset())   //시작 위치 설정
                 .limit(pageable.getPageSize())  //한페이지에 보여지는 행의 개수
                 .fetch();
@@ -41,7 +46,7 @@ public class ELearningSettingRepositoryImpl implements ELearningSettingRepositor
                 .from(QELearningSetting.eLearningSetting)
                 .fetchOne();
 
-        return new PageImpl<>(content, pageable, totalCount);
+        return PageableExecutionUtils.getPage(content, pageable, totalCount);
     }
 
 }
